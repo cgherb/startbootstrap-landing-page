0c76ba637c0e5a3c208d384bf4fc8ff684f0eeb9
CGherb <cgherb@aol.com>
2018-08-14 20:46:19 -0700

    in process

---
+++ js/getDrip.js
@@ -0,0 +1,11 @@
+var _dcq = _dcq || [];
+var _dcs = _dcs || {};
+_dcs.account = '2351812';
+
+(function() {
+    var dc = document.createElement('script');
+    dc.type = 'text/javascript'; dc.async = true;
+    dc.src = '//tag.getdrip.com/2351812.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(dc, s);
+})();
